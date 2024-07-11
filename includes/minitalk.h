8031e67043c50dfbf7ad62ff56f2acb3088a606f
flmarsou <marsouinflorent12@gmail.com>
2024-07-11 10:38:48 +0200

    Update minitalk.h

--- includes/minitalk.h
+++ includes/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 13:33:25 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/07/09 15:32:28 by flmarsou         ###   ########.fr       */
+/*   Updated: 2024/07/11 10:35:46 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,9 @@
 # include <signal.h>
 # include <stdlib.h>
 
-typedef struct s_minitalk
-{
-	char	character;
-	int		bits;
-	char	*string;
-}			t_minitalk;
-
 void		ft_putchr(char c);
 void		ft_putstr(char *str);
 void		ft_putnbr(int nbr);
-void		ft_puterr(char *str);
 int			ft_atoi(char *str);
 int			ft_strlen(char *str);
 
