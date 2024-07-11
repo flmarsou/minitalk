f6548bb0a10fc2d9887631bc4edd7b367f8562e3
flmarsou <marsouinflorent12@gmail.com>
2024-07-11 15:36:18 +0200

    Update client.c

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 15:49:12 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/07/11 10:34:41 by flmarsou         ###   ########.fr       */
+/*   Updated: 2024/07/11 13:15:37 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,18 @@
 
 static void	received(int signum)
 {
-	static unsigned int	size;
-
 	if (signum == SIGUSR1)
-		size++;
-	else
 	{
-		ft_putnbr(size);
-		ft_putchr('\n');
+		ft_putstr("\e[1;32m[✓] - Successfully received the message!\n\e[0m");
 		exit(0);
 	}
 }
 
 static void	sender(int pid, char *str)
 {
-	int	byte;
-	int	i;
-	int	bit;
+	unsigned int	byte;
+	unsigned int	i;
+	unsigned int	bit;
 
 	byte = -1;
 	while (str[++byte])
@@ -43,14 +38,14 @@ static void	sender(int pid, char *str)
 				kill(pid, SIGUSR1);
 			else
 				kill(pid, SIGUSR2);
-			usleep(600);
+			usleep(200);
 		}
 	}
 	i = 8;
 	while (i--)
 	{
 		kill(pid, SIGUSR1);
-		usleep(600);
+		usleep(200);
 	}
 }
 
@@ -61,11 +56,12 @@ int	main(int argc, char **argv)
 		ft_putstr("\e[1;97mUsage:\e[0m ./client <PID> \"Message\"\n");
 		exit(1);
 	}
-	ft_putstr("\e[1;97mByte Sent: ");
-	ft_putnbr(ft_strlen(argv[2]));
-	ft_putstr("\nByte Received: ");
+	else if (ft_strlen(argv[2]) > 1024)
+	{
+		ft_putstr("\e[1;97m[!] - Reached the limit (1024 characters)!\n\e[0m");
+		exit(1);
+	}
 	signal(SIGUSR1, received);
-	signal(SIGUSR2, received);
 	sender(ft_atoi(argv[1]), argv[2]);
 	while (1)
 		pause();
